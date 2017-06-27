bc7f369e5d1f3008e90dcf8424ce15b1d0a092a5
roberto.debarba <roberto.debarba@senior.com.br>
2017-06-27 12:24:32 -0300

    Alterado para rodar consumer no heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web:    java $JAVA_OPTS -jar distributed-flood-monitoring-system-api/target/dependency/webapp-runner.jar --port $PORT distributed-flood-monitoring-system-api/target/*.war
+consumer:    java $JAVA_OPTS -jar --port $PORT distributed-flood-monitoring-system-consumer/target/*.jar
--- distributed-flood-monitoring-system-consumer/src/main/java/br/com/robertodebarba/floodmonitoring/consumer/Consumer.kt
+++ distributed-flood-monitoring-system-consumer/src/main/java/br/com/robertodebarba/floodmonitoring/consumer/Consumer.kt
@@ -8,16 +8,20 @@ object Consumer{
     val RIVERLEVEL = 4
     @JvmStatic fun main(args: Array<String>) {
 
-        println("1 - Nivel da Barragem ")
-        println("2 - Comporta ")
-        println("3 - Estação de chuva ")
-        println("4 - Nivel do rio ")
-        println("Tipo de Consumidor:")
-        val tipo = readLine()?.toInt() ?: 1
+        //println("1 - Nivel da Barragem ")
+        //println("2 - Comporta ")
+        //println("3 - Estação de chuva ")
+        //println("4 - Nivel do rio ")
+        //println("Tipo de Consumidor:")
+        //val tipo = readLine()?.toInt() ?: 1
 
-        if(tipo == DAMLEVEL) DamLevelConsumer().Consume()
-        if(tipo == FLOODGATE) FloodGateConsumer().Consume()
-        if(tipo == RAINFALL) RainFallConsumer().Consume()
-        if(tipo == RIVERLEVEL) RiverLevelConsumer().Consume()
+        //if(tipo == DAMLEVEL) DamLevelConsumer().Consume()
+        //if(tipo == FLOODGATE) FloodGateConsumer().Consume()
+        //if(tipo == RAINFALL) RainFallConsumer().Consume()
+        //if(tipo == RIVERLEVEL) RiverLevelConsumer().Consume()
+        DamLevelConsumer().Consume()
+        FloodGateConsumer().Consume()
+        RainFallConsumer().Consume()
+        RiverLevelConsumer().Consume()
     }
 }
