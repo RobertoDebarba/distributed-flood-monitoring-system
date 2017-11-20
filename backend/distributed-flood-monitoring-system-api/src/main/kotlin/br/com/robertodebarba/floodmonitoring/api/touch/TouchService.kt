0e11b63f71e82f436452a2e157f79062244e9b1b
roberto.debarba <roberto.debarba@senior.com.br>
2017-11-20 12:46:32 -0200

    Implementado envio de email no backend

--- backend/distributed-flood-monitoring-system-api/src/main/kotlin/br/com/robertodebarba/floodmonitoring/api/touch/TouchService.kt
+++ backend/distributed-flood-monitoring-system-api/src/main/kotlin/br/com/robertodebarba/floodmonitoring/api/touch/TouchService.kt
@@ -1,10 +1,33 @@
 package br.com.robertodebarba.floodmonitoring.api.touch
 
+import org.apache.commons.mail.DefaultAuthenticator
+import org.apache.commons.mail.SimpleEmail
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class TouchService {
+
+    @Value("\${email}")
+    private lateinit var email: String
+
+    @Value("\${password}")
+    private lateinit var password: String
+
+    @Value("\${sendTo}")
+    private lateinit var sendTo: String
+
     fun postTouch(touchDTO: TouchDTO) {
-        println("Mensagem: ${touchDTO.message}")
+        SimpleEmail().apply {
+            hostName = "smtp.live.com"
+            isStartTLSEnabled = true
+            setSmtpPort(25)
+            setAuthenticator(DefaultAuthenticator(email, password))
+
+            setFrom(email)
+            subject = "[CLIMA ALERTA] Nova mensagem de ${touchDTO.name}"
+            setMsg("${touchDTO.name} <${touchDTO.email}> enviou uma mensagem:\n\n${touchDTO.message}")
+            addTo(sendTo)
+        }.send()
     }
 }
