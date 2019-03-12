c9e2b69deb67c9ebedc0047462ca32b6788be4fb
robertodebarba <roberto.debarba@gmail.com>
2019-03-12 20:52:51 -0300

    Definido CeopsConsumerApplication como SpringBootApplication

--- backend/distributed-flood-monitoring-system-producer-ceops/src/main/kotlin/br/com/robertodebarba/floodmonitoring/consumer/ceops/CeopsConsumerApplication.kt
+++ backend/distributed-flood-monitoring-system-producer-ceops/src/main/kotlin/br/com/robertodebarba/floodmonitoring/consumer/ceops/CeopsConsumerApplication.kt
@@ -1,11 +1,12 @@
 package br.com.robertodebarba.floodmonitoring.consumer.ceops
 
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableScheduling
 
-
+@SpringBootApplication
 @Configuration
 @EnableScheduling
 @ComponentScan(basePackages = arrayOf(
